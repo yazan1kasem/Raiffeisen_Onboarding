15fbf19b6cbeb98f3eb81d6bba1b3cb134b482bd
simon <simonkotvas9@icloud.com>
2024-12-11 08:39:53 +0100

    neue imports

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,19 @@
 INSERT INTO db_raiffeisen.c_checklist (c_id, c_überschrift, c_abteilungsname, c_position) VALUES ("1", "Safety Checklist", "Facility Management", "Supervisor");
 INSERT INTO db_raiffeisen.c_checklist (c_id, c_überschrift, c_abteilungsname, c_position) VALUES ("2", "IT Equipment Checklist", "IT Support", "Techniker");
+INSERT INTO db_raiffeisen.c_checklist (c_id, c_überschrift, c_abteilungsname, c_position) VALUES ("3", "Security Checklist", "IT Security", "Analyst");
+INSERT INTO db_raiffeisen.c_checklist (c_id, c_überschrift, c_abteilungsname, c_position) VALUES ("4", "Hardware Maintenance Checklist", "Hardware Support", "Techniker");
+INSERT INTO db_raiffeisen.c_checklist (c_id, c_überschrift, c_abteilungsname, c_position) VALUES ("5", "Onboarding Checklist", "Human Resources", "Manager");
+INSERT INTO db_raiffeisen.c_checklist (c_id, c_überschrift, c_abteilungsname, c_position) VALUES ("6", "Training Checklist", "Human Resources", "Trainer");
+INSERT INTO db_raiffeisen.c_checklist (c_id, c_überschrift, c_abteilungsname, c_position) VALUES ("7", "Software Deployment Checklist", "Software Development", "Engineer");
+INSERT INTO db_raiffeisen.c_checklist (c_id, c_überschrift, c_abteilungsname, c_position) VALUES ("8", "Network Setup Checklist", "Networking", "Administrator");
+INSERT INTO db_raiffeisen.c_checklist (c_id, c_überschrift, c_abteilungsname, c_position) VALUES ("9", "Compliance Audit Checklist", "Legal", "Auditor");
+INSERT INTO db_raiffeisen.c_checklist (c_id, c_überschrift, c_abteilungsname, c_position) VALUES ("10", "Project Kickoff Checklist", "Project Management", "Coordinator");
+INSERT INTO db_raiffeisen.c_checklist (c_id, c_überschrift, c_abteilungsname, c_position) VALUES ("11", "Daily Operations Checklist", "Operations", "Supervisor");
+INSERT INTO db_raiffeisen.c_checklist (c_id, c_überschrift, c_abteilungsname, c_position) VALUES ("12", "Customer Support Checkist", "Customer Service", "Agent");
+INSERT INTO db_raiffeisen.c_checklist (c_id, c_überschrift, c_abteilungsname, c_position) VALUES ("13", "Quality Assurance Checklist", "Quality Control", "Inspector");
+INSERT INTO db_raiffeisen.c_checklist (c_id, c_überschrift, c_abteilungsname, c_position) VALUES ("14", "Quality Assurance Checklist", "Quality Control", "Agent");
+
+
 
 INSERT INTO db_raiffeisen.I_Item (I_id, I_gerät, I_administration, I_software, I_suchbegriff, c_i_id) VALUES ("1", "Laptop", "IT Administration", "Windows 10", "laptop", "1");
 INSERT INTO db_raiffeisen.I_Item (I_id, I_gerät, I_administration, I_software, I_suchbegriff, c_i_id) VALUES ("2", "Smartphone", "Mobile Device Management", "iOS 16", "smartphone", "1");
