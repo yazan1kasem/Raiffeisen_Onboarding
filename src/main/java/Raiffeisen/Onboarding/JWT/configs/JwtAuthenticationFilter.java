c832b07be2f0fdb544cecfe35deb62c3bdd5a1dc
yazan204 <118280703+yazan204@users.noreply.github.com>
2024-12-22 19:24:24 +0100

    use secrets from Github and implement a secret key

--- src/main/java/Raiffeisen/Onboarding/JWT/configs/JwtAuthenticationFilter.java
+++ src/main/java/Raiffeisen/Onboarding/JWT/configs/JwtAuthenticationFilter.java
@@ -23,6 +23,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final HandlerExceptionResolver handlerExceptionResolver;
 
     private final JwtService jwtService;
+
     private final UserDetailsService userDetailsService;
 
     public JwtAuthenticationFilter(
