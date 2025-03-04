90138798d1e73b54a8e13a2622dfe00b4d51171b
yazan204 <118280703+yazan204@users.noreply.github.com>
2025-03-04 12:26:55 +0100

    deployen habibi

--- src/main/java/Raiffeisen/Onboarding/JWT/configs/SecurityConfiguration.java
+++ src/main/java/Raiffeisen/Onboarding/JWT/configs/SecurityConfiguration.java
@@ -51,14 +51,17 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // Allow specific origin
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "https://rbiOnboarding.azurewebsites.net"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-
         return source;
     }
+
 }
