203c3160c337f1541ddda49ff384aa33c99bcc88
rbiraiffeisen <rbiraiffeisen25@gmail.com>
2025-03-15 15:19:18 +0100

    Update SecurityConfiguration.java

--- src/main/java/Raiffeisen/Onboarding/JWT/configs/SecurityConfiguration.java
+++ src/main/java/Raiffeisen/Onboarding/JWT/configs/SecurityConfiguration.java
@@ -53,7 +53,9 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-                "https://onboarding-frontend-6lv9.onrender.com"
+                "https://onboarding-frontend-6lv9.onrender.com",
+                "rbionboarding.at",
+                "www.rbionboarding.at"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
