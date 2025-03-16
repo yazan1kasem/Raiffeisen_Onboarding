af3482872373b24fa137a591c8026048d00b86f4
rbiraiffeisen <rbiraiffeisen25@gmail.com>
2025-03-16 14:05:58 +0100

    Update checklist.component.ts

--- Onboarding_client/src/app/checklist/checklist.component.ts
+++ Onboarding_client/src/app/checklist/checklist.component.ts
@@ -39,10 +39,11 @@ export class ChecklistComponent implements OnInit {
       // Filteroptionen aktualisieren
       this.loadingChecklist=false;
       this.abteilungen = [...new Set(data.map(checklist => checklist.abteilungsname))];
+      this.filteredChecklists = data;
+      this.updatePositions();
+      this.updateDepartments();
     });
-    this.filteredChecklists = this.checklists;
-    this.updatePositions();
-    this.updateDepartments();
+
 
   }
 
