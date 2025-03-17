96582a26739a5787b44fe74e75e94cafc2950609
rbiraiffeisen <rbiraiffeisen25@gmail.com>
2025-03-17 10:35:14 +0100

    Update auth.service.ts

--- Onboarding_client/src/app/auth.service.ts
+++ Onboarding_client/src/app/auth.service.ts
@@ -21,6 +21,7 @@ export class AuthService {
   }
 
   login(username: string, password: string): Observable<any> {
+    localStorage.removeItem('token'); 
     return this.http.post(`${this.apiUrl}/login`, { username, password }).pipe(
       tap((response: any) => {
         localStorage.setItem('token', response.token);// Speichere den JWT-Token im Local Storage
